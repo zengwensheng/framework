69b8f152f92ea10ab67cd736879a025151d7602c
zws <zengwensheng>
2018-11-14 17:38:43 +0800

    添加解释器模式

--- design-patterns/src/main/java/com/zws/interpreter/cls/SubExpresstion.java
+++ design-patterns/src/main/java/com/zws/interpreter/cls/SubExpresstion.java
@@ -6,12 +6,10 @@ public class SubExpresstion extends SymbolExpresstion {
 
 	public SubExpresstion(AbstractExpresstion _left, AbstractExpresstion _right) {
 		super(_left, _right);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Float interpreter(HashMap<String, Float> var) {
-		// TODO Auto-generated method stub
 		return super.left.interpreter(var) - super.right.interpreter(var);
 	}
 
