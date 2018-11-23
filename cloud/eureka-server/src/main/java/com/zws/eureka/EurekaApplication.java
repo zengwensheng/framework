9b89c831952026e7e7e0b9ff0c44a470369e46f8
zws <zengwensheng>
2018-11-23 17:23:52 +0800

    添加

---
+++ cloud/eureka-server/src/main/java/com/zws/eureka/EurekaApplication.java
@@ -0,0 +1,12 @@
+package com.zws.eureka;
+
+/**
+ * @author zws
+ * @email 2848392861@qq.com
+ * date 2018/11/23
+ */
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+    }
+}
