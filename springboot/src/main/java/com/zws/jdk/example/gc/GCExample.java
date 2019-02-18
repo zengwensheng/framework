c6018614c71ee80534f4d2895b036a91714ae3f3
zws <zengwensheng>
2019-02-18 18:29:20 +0800

    jvmGC 算法与参数配置

---
+++ springboot/src/main/java/com/zws/jdk/example/gc/GCExample.java
@@ -0,0 +1,18 @@
+package com.zws.jdk.example.gc;
+
+/**
+ * @author zws
+ * @email 2848392861@qq.com
+ * date 2019/2/12
+ *
+ * GC 算法与种类
+ *
+ * GC 算法详见：笔记1.1-java-java基础-jdk-base-GC算法
+ *
+ * 垃圾收集器种类
+ *
+ *
+ *
+ */
+public class GCExample {
+}
