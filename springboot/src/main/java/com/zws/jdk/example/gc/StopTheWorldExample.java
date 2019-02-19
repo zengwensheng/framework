9ea287721970e97f9dd38b8ee3fd9f8fa9c7bdd8
zws <zengwensheng>
2019-02-19 10:08:31 +0800

    stop the world

--- springboot/src/main/java/com/zws/jdk/example/gc/StopTheWorldExample.java
+++ springboot/src/main/java/com/zws/jdk/example/gc/StopTheWorldExample.java
@@ -18,24 +18,29 @@ public class StopTheWorldExample {
     /**
      * 通过集合引用对象，保证对象不被gc回收
      */
-    private List<byte[]> content=new ArrayList<>();
+    private List<byte[]> content = new ArrayList<>();
+
+    public static final long starttime = System.currentTimeMillis();
+
 
     /**
      * -Xms512m -Xmx512m -Xmn4m  -XX:+UseSerialGC
+     *
      * @param args
      */
     public static void main(String[] args) {
-        StopTheWorldExample stw=new StopTheWorldExample();
+        new PrintThread().start();
+        StopTheWorldExample stw = new StopTheWorldExample();
         stw.start();
     }
 
     private void start() {
-        while(true){
+        while (true) {
             try {
                 content.add(new byte[1024]);
             } catch (OutOfMemoryError e) {
                 //在不可以分配的时候，进行清理部分空间,继续运行，这样会很快产生下一次垃圾回收
-                for(int i=0;i<1024;i++){
+                for (int i = 0; i < 1024; i++) {
                     content.remove(i);
                 }
 
@@ -43,8 +48,25 @@ public class StopTheWorldExample {
 
         }
     }
-}
 
+
+
+}
+class PrintThread extends Thread{
+    public static final long starttime=System.currentTimeMillis();
+    @Override
+    public void run(){
+        try{
+            while(true){
+                long t=System.currentTimeMillis()-starttime;
+                System.out.println("time:"+t);
+                Thread.sleep(100);
+            }
+        }catch(Exception e){
+
+        }
+    }
+}
 
 
 
